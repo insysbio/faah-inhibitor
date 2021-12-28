b064d0e830753a677ff3531f2e2f33d97ed516cc
Evgeny Metelkin <metelkin@insysbio.com>
2021-12-28 18:52:22 +0300

    minor

--- julia/run.jl
+++ julia/run.jl
@@ -2,7 +2,7 @@ using HetaSimulator, Plots
 
 model = load_jlmodel("./dist/julia/model.jl")
 
-res = Scenario(model; tspan = (1,340)) |> sim
+res = Scenario(model; tspan = (0,340)) |> sim
 
 plotd = plot(res; vars = [:P_p, :A_p, :O_p])
 savefig(plotd, "julia-plot-1.png")
